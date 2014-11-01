354fbc2d2c7686789c30fa943842007ff1c0acc9
zoftrix <zofcos@yahoo.fr>
2014-11-01 14:53:30 +0100

    Add files

---
+++ include/php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <title>B2IRH - GIT</title>
+    </head>
+
+    <body>
+        <!-- Corps de la page -->
+        <h1>Page de test pour le projet B2IRH </h1>
+
+        <p>Bienvenue dans GIT ! 
+        <p>By Zoftrix
+
+    </body>
+</html>
